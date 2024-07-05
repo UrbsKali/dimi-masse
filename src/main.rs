58791e89f91aafa45f03b5a3bcbbdbffcfce242b
UrbsKali <urbain.lantres@tutanota.com>
2024-07-05 15:41:01 +0200

    It WORKS

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Pointer;
 use cortex_m::delay;
+use stm32h7xx_hal::rcc::ResetEnable;
 // Core
+use core::panic::PanicInfo;
 use cortex_m_rt::entry;
 
 // Device
@@ -10,9 +13,6 @@ use hx711_spi::Hx711;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32h7xx_hal::nb::block;
 use stm32h7xx_hal::{delay::Delay, pac, prelude::*, spi};
-
-// panic handler
-use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
@@ -23,12 +23,16 @@ fn main() -> ! {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
+    rprintln!("Peripherals taken!");
+
     // On configure l'alimentation de la carte (PWR)
     // VOS0 (1.4V) est le mode de consommation le plus haut, qui permet d'atteindre la plus haut fréquence de fonctionnement
     // cf Notion pour plus d'infos
     // SYSFG : System Configuration Controller
     let pwr = dp.PWR.constrain();
-    let pwrcfg = pwr.vos0(&dp.SYSCFG).freeze();
+    let pwrcfg = pwr.vos1().freeze();
+
+    rprintln!("Power configured!");
 
     // On configure les clocks du microcontrôleur
     // RCC : Reset and Clock Control
@@ -37,13 +41,16 @@ fn main() -> ! {
     // pclk1 : fréquence du bus 1 (Périphériques)
     let rcc = dp.RCC.constrain();
     let ccdr = rcc
-        .sys_ck(96.MHz())
-        .pclk1(48.MHz())
+        .sys_ck(200.MHz()) // Implies pll1_p_ck
+        // For non-integer values, round up. `freeze` will never
+        // configure a clock faster than that specified.
+        .pll1_q_ck(33_333_334.Hz())
         .freeze(pwrcfg, &dp.SYSCFG);
 
     // On récupère les clocks créés par le système, qui ne sont pas
     // forcement les mêmes que celles demandées
-    let clocks = ccdr.clocks;
+
+    rprintln!("Clocks configured!");
 
     // On récupère les périphériques GPIO
     let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
@@ -52,18 +59,25 @@ fn main() -> ! {
     let miso = gpioc.pc11.into_alternate();
     //let mosi = gpioc.pc12.into_alternate(); // Pas besoin de MOSI pour le HX711
 
+    rprintln!("GPIO configured!");
+
     // On configure le SPI
-    let mut spi = dp.SPI3.spi(
+    let mut spi: spi::Spi<_, _, u8> = dp.SPI3.spi(
         (sck, miso, spi::NoMosi),
-        spi::MODE_0,
-        3.MHz(),
+        spi::MODE_1,
+        1.MHz(),
         ccdr.peripheral.SPI3,
-        &clocks,
+        &ccdr.clocks,
     );
+    rprintln!("SPI configured!");
 
     let mut hx711 = Hx711::new(spi);
+    hx711.reset().unwrap();
+    hx711.set_mode(hx711_spi::Mode::ChAGain128).unwrap(); // x128 works up to +-20mV
 
-    let mut delay = Delay::new(cp.SYST, clocks);
+    rprintln!("Init done!");
+
+    let mut delay = Delay::new(cp.SYST, ccdr.clocks);
 
     loop {
         // get data from hx711
@@ -72,4 +86,11 @@ fn main() -> ! {
         // wait 1s
         delay.delay_ms(1000_u32);
     }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    rprintln!("\n#--------- Panic! ---------#\n");
+    rprintln!("{}", _info);
+    loop {}
 }
