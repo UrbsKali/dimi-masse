3705200f092d9ed5f945752b07d77299ff8060e8
UrbsKali <urbain.lantres@tutanota.com>
2024-07-15 18:26:39 +0200

    yep

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use core::panic::PanicInfo;
 use cortex_m_rt::entry;
 
 // Device
-use hx711_spi::Hx711;
+use hx711::Hx711;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32h7xx_hal::nb::block;
 use stm32h7xx_hal::{delay::Delay, pac, prelude::*, spi};
@@ -44,7 +44,7 @@ fn main() -> ! {
         .sys_ck(200.MHz()) // Implies pll1_p_ck
         // For non-integer values, round up. `freeze` will never
         // configure a clock faster than that specified.
-        .pll1_q_ck(33_333_334.Hz())
+        .pll1_q_ck(10.MHz())
         .freeze(pwrcfg, &dp.SYSCFG);
 
     // On récupère les clocks créés par le système, qui ne sont pas
@@ -55,35 +55,20 @@ fn main() -> ! {
     // On récupère les périphériques GPIO
     let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
 
-    let sck = gpioc.pc10.into_alternate();
-    let miso = gpioc.pc11.into_alternate();
-    //let mosi = gpioc.pc12.into_alternate(); // Pas besoin de MOSI pour le HX711
-
-    rprintln!("GPIO configured!");
-
-    // On configure le SPI
-    let mut spi: spi::Spi<_, _, u8> = dp.SPI3.spi(
-        (sck, miso, spi::NoMosi),
-        spi::MODE_1,
-        1.MHz(),
-        ccdr.peripheral.SPI3,
-        &ccdr.clocks,
-    );
-    rprintln!("SPI configured!");
-
-    let mut hx711 = Hx711::new(spi);
-    hx711.reset().unwrap();
-    hx711.set_mode(hx711_spi::Mode::ChAGain128).unwrap(); // x128 works up to +-20mV
-
+    let dout = gpioc.pc11.into_floating_input().into_pull_down_input();
+    let pd_sck = gpioc.pc10.into_push_pull_output();
+    let mut hx711 = Hx711::new(Delay::new(cp.SYST, ccdr.clocks), dout, pd_sck).unwrap();
+    /*
+        let mut hx711 = Hx711::new(spi);
+        hx711.reset().unwrap();
+        hx711.set_mode(hx711_spi::Mode::ChAGain128).unwrap(); // x128 works up to +-20mV
+    */
     rprintln!("Init done!");
-
-    let mut delay = Delay::new(cp.SYST, ccdr.clocks);
-    let N = 8;
+    let N = 4;
     let mut tare = 0;
 
     for _ in 0..N {
-        tare += block!(hx711.read()).unwrap();
-        delay.delay_ms(100_u16);
+        tare += block!(hx711.retrieve()).unwrap();
     }
     tare = tare / N;
     rprintln!("Tare: {}", tare);
@@ -92,7 +77,7 @@ fn main() -> ! {
         // get data from hx711
         let mut data = 0;
         for _ in 0..N {
-            data += block!(hx711.read()).unwrap();
+            data += block!(hx711.retrieve()).unwrap();
         }
         data = data / N;
         data -= tare;
